816a8b741cb7829c62e7e8590cffaac84b40fb1a
Luigitapia3003 <Carlaz020400@gmail.com>
2024-02-07 08:46:53 -0600

    Salto del pajaro
    Salto

--- Assets/Scripts/FlappyBIrd/Bird.cs
+++ Assets/Scripts/FlappyBIrd/Bird.cs
@@ -1,13 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Bird : MonoBehaviour
 {
+    private Rigidbody2D rb;
+    [SerializeField]
+    private float jumpForce = 5f;
+    private Bird_inputs bird;
+
+    private void Awake()
+    {
+        bird = new Bird_inputs();
+        bird.Enable();
+    }
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
+        bird.Standard.Jump.performed += Move;
     }
 
     // Update is called once per frame
@@ -15,4 +28,12 @@ public class Bird : MonoBehaviour
     {
         
     }
+
+
+    private void Move(InputAction.CallbackContext context)
+    {
+        rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+        Debug.Log("SALTÓ");
+    }
+
 }
